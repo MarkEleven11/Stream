1105b61c639325411ed16c3b27fe95b6be414460
Александр <alexakr1993@gmail.com>
2023-02-14 11:14:12 +0400

    Сделано

--- src/Main.java
+++ src/Main.java
@@ -8,20 +8,32 @@ public class Main {
     public static void main(String[] args) {
         numbers(List.of(5,3,8,11,2,6));
         findMinMax(Stream.of(5,1,6,-8), Integer::compareTo, (min, max) -> System.out.println("Min " + min + ". Max " + max));
+        findMinMax1(Stream.of(), Integer::compareTo, (min, max) -> System.out.println("Min " + min + ". Max " + max));
 
     }
     public static <T> void findMinMax (Stream<? extends T> stream,
                                        Comparator<? super T> order,
                                        BiConsumer<? super T, ? super T> minMaxConsumer) {
         List<? extends T> list = stream.sorted(order).collect(Collectors.toList());
-        if (list.isEmpty() ||) {
-            System.out.println("Данные отсутствуют. Введите данные");
-        } else
         minMaxConsumer.accept(list.get(0), list.get(list.size()- 1));
+    }
+
+    public static <T> void findMinMax1(
+            Stream<? extends T> stream,
+            Comparator<? super T> order,
+            BiConsumer<? super T, ? super T> minMaxConsumer) {
+        List<? extends T> list = stream.sorted(order).collect(Collectors.toList());
+        if (list.isEmpty()) {
+            minMaxConsumer.accept(null, null);
+        } else {
+            minMaxConsumer.accept(list.get(0), list.get(list.size() - 1));
+        }
     }
 
     public static void numbers (List<Integer> list){
       long count = list.stream().filter(x -> x % 2 == 0).count();
         System.out.println("Количество четных чисел " + count);
     }
+
+
 }
